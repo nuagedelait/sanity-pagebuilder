571fd13a158bc04c5a8ccf167b364aa109b93010
nuagedelait <oliver@nuagedelait.com>
2024-01-24 01:26:01 +0100

    add multilines block

--- src/schemas/Blocks/index.ts
+++ src/schemas/Blocks/index.ts
@@ -2,6 +2,7 @@ import menu from "./menu"
 import layout from "./layout"
 import grid from "./grid"
 import tabs from "./tabs"
+import multilines from "./multilines"
 
-const blocks: any[] = [menu, layout, grid, tabs]
+const blocks: any[] = [menu, layout, grid, tabs, multilines]
 export default blocks
---
+++ src/schemas/Blocks/multilines.ts
@@ -0,0 +1,64 @@
+import { defineType } from 'sanity';
+import block, { prefix } from './block';
+import { FieldDefinition, SlugOptions } from 'sanity';
+
+const maxLength = 20;
+
+export default defineType({
+    name: 'multilinesBlocks',
+    title: prefix + 'Multilines',
+    type: 'document',
+    fields: [
+        ...block.filter( (field:FieldDefinition) => field.name !== 'title' && field.name !== 'description').map( (field:FieldDefinition) => {
+            if(field.name === 'slug'){
+                (field.options as SlugOptions).source = (doc, options) => {
+                    const parent = options.parent as any;
+                    const allLines = parent.lines.reduce( (acc:string, line:{text:string}) => acc+= line.text+ ' ','');
+                    const title = allLines.length > maxLength ? `${allLines.slice(0,maxLength)}...` : allLines
+                    return `${title}${parent.lang ? `-${parent.lang}` : ''}`;
+                }   
+            }
+            return field;
+        } ),
+        {
+            title: 'Lines',
+            name: 'lines',
+            type: 'array',
+            of: [
+                {
+                    type: 'object',
+                    preview: {
+                        select: {
+                            title: 'text',
+                          }
+                    },
+                    fields: [
+                        {
+                            name: 'class',
+                            type: 'string'
+                        },
+                        {
+                            name: 'text',
+                            type: 'string'
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    preview: {
+        select: {
+            lines: 'lines',
+            media: 'image'
+        },
+        prepare(selection) {
+            const { lines, media } = selection;
+            const allLines = lines.reduce( (acc:string, line:{text:string}) => acc+= line.text+ ' ','');
+            const title = allLines.length > maxLength ? `${allLines.slice(0,maxLength)}...` : allLines
+            return {
+                title: `Multilines | ${title}`,
+                media
+            }
+        }
+    }
+})
